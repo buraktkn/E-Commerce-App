3ebc2f0893239ab4f2ed2236625a737a8db37c1c
buraktkn <burak.tekin@std.medipol.edu.tr>
2025-05-28 00:36:11 +0300

    Load More ile yükleme işlemi yapıldı

--- client/src/Pages/Products.js
+++ client/src/Pages/Products.js
@@ -1,28 +1,64 @@
-import { Grid } from "@chakra-ui/react";
+import { Grid, Box, Button, Flex } from "@chakra-ui/react";
 import Card from "../Components/Card";
 import React from "react";
-import { useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery } from "@tanstack/react-query";
 
 export default function Products() {
-    const fetchProducts = async () => {
-        const res = await fetch("http://localhost:4000/product");
+    const fetchProducts = async ({pageParam = 0}) => {
+        const res = await fetch(`http://localhost:4000/product?page=${pageParam}`);
         if (!res.ok) {throw new Error(`HTTP error! status: ${res.status}`);}
         return res.json();
+        
       };
-      const {data: products, isLoading, error} = useQuery({
+      const {
+        data,
+        error,
+        isLoading,
+        fetchNextPage,
+        hasNextPage,
+        isFetching,
+        isFetchingNextPage,
+        status
+        } = useInfiniteQuery({
         queryKey: ["product"],
         queryFn: fetchProducts,
+        initialPageParam: 0,
+        getNextPageParam: (lastGroup, allGroups) => {
+            const morePagesExits = lastGroup?.length === 12
+            if(!morePagesExits){return}
+            return allGroups.length + 1;
+        }
       });
       if (isLoading) return <div>Loading...</div>;
       if (error) return <div>An error has occured: {error.message}</div>;
+      console.log("Data",data);
+      
 
   return (
     <div>
-      <Grid templateColumns="repeat(5, 1fr)" gap="5">
-        {products.map((item, key) => (
-          <Card key={key} item={item} />
+      <Grid templateColumns="repeat(5, 1fr)" gap="5"> 
+        {   data.pages.map((group,i)=>(
+                <React.Fragment key={i}>
+                    {group.map(item => (
+                        <Box w="100%" key={item._id}><Card item={item}/></Box>
+                    ))}
+                </React.Fragment>
         ))}
       </Grid>
+      <Flex justifyContent={"center"} m={10}>
+        <Button 
+          onClick={() => fetchNextPage()}
+          isLoading={true}
+          disabled={!hasNextPage || isFetching}
+        >
+          {isFetchingNextPage
+            ? 'Loading more...'
+            : hasNextPage
+              ? 'Load More'
+              : 'Nothing more to load'}
+        </Button>
+      <div>{isFetching && !isFetchingNextPage ? 'Fetching...' : null}</div>
+      </Flex >
     </div>
   );
 }
