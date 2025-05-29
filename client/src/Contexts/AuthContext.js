3e9bda4a5bcd08ec304d26fa71f5b03a14f02a5c
buraktkn <burak.tekin@std.medipol.edu.tr>
2025-05-29 20:14:40 +0300

    Auth Context Kuruldu

---
+++ client/src/Contexts/AuthContext.js
@@ -0,0 +1,18 @@
+import { createContext, useContext, useState } from "react";
+
+const AuthContext = createContext();
+
+const AuthProvider = ({children}) =>{
+    const [user, setUser] = useState(null);
+    const [loggedIn, setLoggedIn] =useState(false);
+
+    const login = (data) =>{
+        setLoggedIn(true);
+        setUser(data);
+    }
+    const values = { loggedIn, user, login };
+    return <AuthContext.Provider value={values}>{children}</AuthContext.Provider>
+};
+
+const useAuth = () => useContext(AuthContext);
+export {AuthProvider, useAuth};
